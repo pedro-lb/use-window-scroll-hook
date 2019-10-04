0a2043d18c610cc82d0524f8631c18b729b041d9
Pedro Bini <dev.pedrobini@gmail.com>
2019-10-04 18:44:39 -0300

    Add onlyOnScrollEnd event option

--- src/useWindowScroll.js
+++ src/useWindowScroll.js
@@ -9,16 +9,21 @@ const getScroll = () => ({
  * Hook that monitors window scroll, and updates the object
  * at the end of each window scroll. It returns the current
  * offset X and Y position.
+ * @param {If true, fires the event only when the user stops scrolling.} onlyOnScrollEnd
  */
-export default function useWindowScroll() {
+export default function useWindowScroll(onlyOnScrollEnd = true) {
   const [windowScroll, setWindowScroll] = useState(getScroll());
 
   useEffect(() => {
     let scrollId;
 
     const handleScroll = () => {
-      clearTimeout(scrollId);
-      scrollId = setTimeout(() => setWindowScroll(getScroll()), 200);
+      if (onlyOnScrollEnd) {
+        clearTimeout(scrollId);
+        scrollId = setTimeout(() => setWindowScroll(getScroll()), 200);
+      } else {
+        setWindowScroll(getScroll());
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
